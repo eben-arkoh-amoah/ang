47df7324fc78ce1f63c80996356d1979403c4571
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-11-29 02:26:16 +1200

    created the reset password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './authentication/auth/auth.component';
 import { LoginComponent } from './authentication/auth/login/login.component';
 import { FrgtPasswordComponent } from './authentication/auth/frgt-password/frgt-password.component';
+import { ResetComponent } from './authentication/auth/reset/reset.component';
+
 export const routes: Routes = [
     {path: '', redirectTo: '/auth', pathMatch:'full'},
     {path: 'auth', component: AuthComponent,
      children: [
         {path: '', redirectTo: '/auth/login', pathMatch:'full'},
         {path: 'login', component: LoginComponent},
-        {path: 'forgot-password', component: FrgtPasswordComponent}
+        {path: 'forgot-password', component: FrgtPasswordComponent},
+        {path: 'reset-password', component: ResetComponent}
      ]
     }
 ];
---
+++ src/app/authentication/auth/reset/reset.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import {FormsModule, NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-reset',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './reset.component.html',
+  styleUrl: '../login/login.component.css'
+})
+export class ResetComponent {
+
+  onFormSubmit(form: NgForm) {
+
+    if(form.invalid){
+      return;
+    }
+       const LoginCredentials = {
+        password:  form.value.password1,
+       }
+    
+       console.log(LoginCredentials)
+      }
+}
