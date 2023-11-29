ba7a4c366e5b737ea49531eb73fed1cae2d89fa4
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-11-30 10:30:20 +1200

    created the dashboard content component

---
+++ src/app/dsh-content/dsh-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dsh-content',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dsh-content.component.html',
+  styleUrl: './dsh-content.component.css'
+})
+export class DshContentComponent {
+
+}
