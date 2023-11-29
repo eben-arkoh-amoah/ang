ab41e0f633b7fddc6d4e5a7f528ba6c19625053b
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-11-29 22:58:19 +1200

    created the wildcard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { AuthComponent } from './authentication/auth/auth.component';
 import { LoginComponent } from './authentication/auth/login/login.component';
 import { FrgtPasswordComponent } from './authentication/auth/frgt-password/frgt-password.component';
 import { ResetComponent } from './authentication/auth/reset/reset.component';
+import { WildcardComponent } from './wildcard/wildcard.component';
+
 
 export const routes: Routes = [
     {path: '', redirectTo: '/auth', pathMatch:'full'},
     {path: 'auth', component: AuthComponent,
      children: [
         {path: '', redirectTo: '/auth/login', pathMatch:'full'},
-        {path: 'login', component: LoginComponent},
-        {path: 'forgot-password', component: FrgtPasswordComponent},
-        {path: 'reset-password', component: ResetComponent}
+        {path: 'login', component: LoginComponent, title: 'login'},
+        {path: 'forgot-password', component: FrgtPasswordComponent, title:'forgot passowrd'},
+        {path: 'reset-password', component: ResetComponent, title: 'reset password'}
      ]
-    }
+    },
+    {path: "**", component: WildcardComponent, title: '404'}
 ];
---
+++ src/app/wildcard/wildcard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-wildcard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wildcard.component.html',
+  styleUrl: './wildcard.component.css'
+})
+export class WildcardComponent {
+
+}
