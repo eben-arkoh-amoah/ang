9a9e92640b69b3eadf65beff39a2444b25cc06b8
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-12-03 00:22:01 +1200

    completed the layout of the staff basic info page

--- src/app/basic-staff-info/basic-staff-info.component.ts
+++ src/app/basic-staff-info/basic-staff-info.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-staff-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './basic-staff-info.component.html',
   styleUrl: './basic-staff-info.component.css'
 })
 export class BasicStaffInfoComponent {
+  departments = [
+    {value: "Engineering Department"},
+    {value: "No Department"}
+  ]
+  selectedDepartment= this.departments[0];
 
+  genders = [
+    {value: "Female"},
+    {value: "Male"},
+    {value: "Other"}
+  ]
+
+  selectedGender = this.genders[0];
 }
