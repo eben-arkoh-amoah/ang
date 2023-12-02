5fd4c0cc8815178667a003b4f3de3d8eacebec41
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-12-02 23:42:26 +1200

    created the layout of the add-staff

--- src/app/add-staff/add-staff.component.ts
+++ src/app/add-staff/add-staff.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-add-staff',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet ],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './add-staff.component.html',
   styleUrl: './add-staff.component.css'
 })
 export class AddStaffComponent {
-
+  
 }
